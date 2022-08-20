3d2f9be314cc733e7e8342e818d38df0a7ef426a
MarusDod <markitos.2001@hotmail.com>
2022-08-20 23:54:14 +0100

    xmrigged no fees

--- xmrig/default.nix
+++ xmrig/default.nix
@@ -1,38 +1,22 @@
 {pkgs ? import <nixpkgs> {}}:
 
 #let
-let
-    donate-level = 1;
-    user ="42AsQrkti7sBS1FtiJd2WrZA4i2PZUos68K76aKgexPr6uuodiG8MKb1VBJX82GHJ3e6GKFYrUgL1Jecussv13y3BcscGX8";
-    url = "pool.minexmr.com:443";
-    coin = "monero";
-     threads = 8;
-    configFile = pkgs.writeText "config.json" (pkgs.callPackage ./config.nix {inherit donate-level user url coin threads;});
-
-in  with pkgs; stdenv.mkDerivation rec {
+ with pkgs; stdenv.mkDerivation rec {
     pname = "xmrigged";
     version = "6.18.0";
 
     src = fetchFromGitHub {
-      owner = "xmrig";
-      repo = "xmrig";
-      rev = "v${version}";
-      sha256 = "vYXDQSEhPi/jxCO6pxOJ1q0AoBVVRU9vErtJLq90ltk=";
+      owner = "MarusDod";
+      repo = "xmrig-zerofee";
+      rev = "master";
+      sha256 = "bov0cGVb7IkaRm62NjYqmv2+ED+dosNsg+OKV7hJ4Ps=";
     };
 
     nativeBuildInputs = [ cmake ];
     buildInputs = [ libuv libmicrohttpd openssl hwloc makeWrapper ];
 
     installPhase = ''
-      mkdir -p $out/etc/${pname}
-      cp ${configFile} $out/etc/${pname}/config.json
-
-      install -vD xmrig $out/bin/${pname}
-    '';
-
-    postFixup = ''
-      wrapProgram $out/bin/${pname} \
-        --add-flags "-c $out/etc/${pname}/config.json"
+      install -vD xmrig $out/bin/xmrig
     '';
 
     meta = with lib; {
